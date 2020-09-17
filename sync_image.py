ca17b8e48ef658b294432da27a50b1f89add646f
kenwoodjw <blackxin55+@gmail.com>
2020-09-17 17:31:22 +0800

    Update sync_image.py

--- sync_image.py
+++ sync_image.py
@@ -17,7 +17,8 @@ def pull_image():
     for name in name_list:
         new_name = "kenwood/" + name.split("/")[-1]
         subprocess.call("docker pull {}".format(name), shell=True)
-        subprocess.call("docker tag {} {}".format(name, new_name), shell=True)
+        subprocess.call("echo docker tag {0} {1}".format(name, new_name), shell=True)
+        subprocess.call("docker tag {0} {1}".format(name, new_name), shell=True)
         subprocess.call("docker login -u kenwood -p qwer1234", shell=True)
         subprocess.call("docker push {}".format(new_name), shell=True)
         
